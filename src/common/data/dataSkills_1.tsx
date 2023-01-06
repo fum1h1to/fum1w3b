06dd636c7bea4dc6fadf18000ea7b370a5122411
Fumihito Sudo <75571344+fum1h1to@users.noreply.github.com>
2023-01-06 22:13:18 +0900

    ちょっと修正

---
+++ src/common/data/dataSkills_1.tsx
@@ -0,0 +1,90 @@
+import { icon_arduino, icon_bootstrap, icon_css3, icon_django, icon_gulp, icon_html5, icon_java, icon_jquery, icon_js, icon_mui, icon_node, icon_processing, icon_python, icon_react, icon_sass, icon_ts, icon_flask, icon_m5stack } from 'common/imgs/top/icon';
+
+const skilldata_1 = [
+  {
+    name: "HTML5",
+    imgsrc: icon_html5,
+  },
+  {
+    name: "CSS3",
+    imgsrc: icon_css3,
+  },
+  {
+    name: "SASS",
+    imgsrc: icon_sass,
+  },
+  {
+    name: "Bootstrap5",
+    imgsrc: icon_bootstrap,
+  },
+  {
+    name: "JavaScript",
+    imgsrc: icon_js,
+  },
+  {
+    name: "jQuery",
+    imgsrc: icon_jquery,
+  },
+  {
+    name: "Node.js",
+    imgsrc: icon_node,
+  },
+  {
+    name: "Gulp.js",
+    imgsrc: icon_gulp,
+  },
+  {
+    name: "TypeScript",
+    imgsrc: icon_ts,
+  },
+  {
+    name: "React",
+    imgsrc: icon_react,
+  },
+  {
+    name: "Material UI",
+    imgsrc: icon_mui,
+  },
+  // {
+  //   name: "PHP",
+  //   imgsrc: icon_php,
+  // },
+  {
+    name: "Python3",
+    imgsrc: icon_python,
+  },
+  {
+    name: "Django",
+    imgsrc: icon_django,
+  },
+  {
+    name: "Flask",
+    imgsrc: icon_flask,
+  },
+  {
+    name: "Java",
+    imgsrc: icon_java,
+  },
+  {
+    name: "Processing",
+    imgsrc: icon_processing,
+  },
+  // {
+  //   name: "C/C++",
+  //   imgsrc: icon_cpp,
+  // },
+  // {
+  //   name: "C#",
+  //   imgsrc: icon_c_sharp,
+  // },
+  {
+    name: "Arduino",
+    imgsrc: icon_arduino,
+  },
+  {
+    name: "M5Stack",
+    imgsrc: icon_m5stack,
+  },
+]
+
+export default skilldata_1;
---
+++ src/common/data/dataSkills_2.tsx
@@ -0,0 +1,58 @@
+import { icon_burpsuite, icon_docker, icon_firebase, icon_git, icon_github,  icon_httpd, icon_kali, icon_nginx, icon_postgres, icon_svn, icon_virtualbox, icon_vscode, icon_wireshark } from 'common/imgs/top/icon';
+
+const skilldata_2 = [
+  {
+    name: "VSCode",
+    imgsrc: icon_vscode,
+  },
+  {
+    name: "Git",
+    imgsrc: icon_git,
+  },
+  {
+    name: "GitHub",
+    imgsrc: icon_github,
+  },
+  {
+    name: "SVN",
+    imgsrc: icon_svn,
+  },
+  {
+    name: "Firebase",
+    imgsrc: icon_firebase,
+  },
+  {
+    name: "VirtualBox",
+    imgsrc: icon_virtualbox,
+  },
+  {
+    name: "Docker",
+    imgsrc: icon_docker,
+  },
+  {
+    name: "Kali Linux",
+    imgsrc: icon_kali,
+  },
+  {
+    name: "Burp Suite",
+    imgsrc: icon_burpsuite,
+  },
+  {
+    name: "Wireshark",
+    imgsrc: icon_wireshark,
+  },
+  {
+    name: "PostgreSQL",
+    imgsrc: icon_postgres,
+  },
+  {
+    name: "Nginx",
+    imgsrc: icon_nginx,
+  },
+  {
+    name: "Apache httpd",
+    imgsrc: icon_httpd,
+  },
+]
+
+export default skilldata_2;
